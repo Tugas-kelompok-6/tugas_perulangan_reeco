f8df280cdc46ee0d0e8cd8e705f05b0840193085
rikooorn09 <rikorn09@gmail.com>
2022-02-02 14:39:18 +0700

    reeco

---
+++ for.js
@@ -0,0 +1,4 @@
+var forOf = [6, 2, 4, 34, 5, 25];
+for (var inn in forOf) {
+  console.log(inn);
+}
---
+++ while.js
@@ -0,0 +1,11 @@
+var NilaiAwal = 4;
+while (NilaiAwal = 5) {
+    console.log('hai1 ' + NilaiAwal + 'x');
+NilaiAwal++;
+}       
+
+var NilaiAwal = 7;
+while (NilaiAwal = 5) {
+    console.log('hai1 ' + NilaiAwal + 'x');
+NilaiAwal--;
+}       
